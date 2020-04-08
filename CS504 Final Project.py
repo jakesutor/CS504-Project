d10383c761c49f05f08386f6e109d8b2118a39df
jakesutor <jake.sutor@gmail.com>
2020-04-07 22:35:22 -0400

    Corrected data types
    Trying to merge this file again as it didn't work properly previously.
    
    This just corrects the data types.

--- CS504 Final Project.py
+++ CS504 Final Project.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 24 21:20:05 2020
@@ -7,43 +13,113 @@ Created on Tue Mar 24 21:20:05 2020
 
 import pandas as pd
 
-stats = pd.read_csv('Seasons_Stats.csv')
+stats = pd.read_csv('Season_Stats.csv')
 
 # only grabbing years 2007-2018 (train on 2007-2017, test on 2018)
-stats = stats[stats['Year'] > 2006].reset_index(drop=True) 
+stats = stats[stats['Year'] > 2005].reset_index(drop=True) 
 
 # dropping these two columns which are both entirely blank
 stats = stats.drop(columns = ['blanl','blank2','Unnamed: 0'])
 
+
+# In[7]:
+
+
+stats.head()
+
+
+# In[12]:
+
+
 # group by year first, then player, then team
 # this will let us see which players had multiple teams in a single year
 # TOT value for teams is an aggregation of all the team data for the player
-teams = stats.groupby(['Year','Player','Tm']).sum()
+teams = stats.groupby(['Year','Player','Tm','ALL_STAR']).sum()
 teams.head()
+
+
+# In[13]:
+
 
 # resetting the index will preserve the return order of the rows
 # but the columns are treated a
 teams = teams.reset_index()
 teams.head(10)
 
+
+# In[19]:
+
+
 cols = teams.columns.values.tolist()
+
+
+# In[15]:
+
 
 # create a blank dataframe using the column values of Teams dataframe
 # we are doing this so we can append values to it
 TOT = pd.DataFrame(columns=cols)
+
+
+# In[16]:
+
 
 # this will give us all the rows where team == TOT
 for i in range(len(teams)):
     if(teams['Tm'][i]=='TOT'):
         TOT.loc[i] = teams.loc[i]
 
-        # now there are no duplicate values. 
-# we only have the TOT values for each player in a given season
-noDupes = teams[teams['Player'].isin(TOT['Player'])==False]
 
-finalTeams = pd.concat([noDupes, TOT])
+# In[18]:
+
+
+TOT.head()
+
+
+# In[20]:
+
+
+teams = teams.drop_duplicates(subset=['Player','Year'],keep=False)
+
+
+# In[21]:
+
+
+finalTeams = pd.concat([teams,TOT])
 finalTeams = finalTeams.sort_values(by=['Year','Player'])
+finalGrouped = finalTeams.groupby(['Year','Player','Tm','ALL_STAR']).sum()
 
-final = finalTeams.groupby(['Year','Player','Tm']).sum()
-final.head()
-finalTeams.head()
+
+# In[35]:
+
+
+finalTeams = finalTeams.astype({'Year': 'int64', 'ALL_STAR':'bool','Age': 'int64','G':'int64','GS':'int64',
+                                'MP':'int64','FG':'int64','FGA':'int64','3P':'int64','3PA':'int64',
+                                '2P':'int64','2PA':'int64','FT':'int64','FTA':'int64','ORB':'int64',
+                                'DRB':'int64','TRB':'int64','AST':'int64','STL':'int64','BLK':'int64',
+                                'TOV':'int64','PF':'int64','PTS':'int64'})
+finalTeams.dtypes
+
+
+# In[22]:
+
+
+finalTeams[finalTeams['Player']=='Isaiah Thomas']
+
+
+# In[23]:
+
+
+finalTeams.to_csv('Final_Season_Stats.csv')
+
+
+# # EDA/ QA
+
+# In[ ]:
+
+
+cols = stats.columns
+for i in cols:
+    print(i)
+    print(stats[i].unique())
+
