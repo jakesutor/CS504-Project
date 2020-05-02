46583892eb5528ee30de067b13afc36e2c42cb7c
jakesutor <jake.sutor@gmail.com>
2020-05-02 10:57:47 -0400

    Updated model and visualizations

--- CS504 Final Project.py
+++ CS504 Final Project.py
@@ -1,8 +1,6 @@
-
 # -*- coding: utf-8 -*-
 """
 Created on Fri Apr 17 18:54:09 2020
-
 @author: prahi
 """
 import pandas as pd
@@ -21,7 +19,7 @@ from sklearn.ensemble          import RandomForestClassifier, AdaBoostClassifier
 from sklearn.preprocessing     import StandardScaler
 
 
-stats = pd.read_csv('Season_Stats.csv')
+stats = pd.read_csv(r'C:\Users\jakes\Documents\GitHub\CS504-Project\Season_Stats.csv')
 
 # only grabbing years 2006-2017 (train on 2007-2016, test on 2017)
 stats = stats[stats['Year'] > 2005].reset_index(drop=True)
@@ -219,6 +217,7 @@ plt.text(10, 0.5, 'Minimum 200 minutes played')
 
 
 teamsFiltered2.dtypes
+teamsFiltered2['PPG'] = teamsFiltered2['PTS']/teamsFiltered2['G']
 
 #remove 2017 season from test set, and remove y-values to prepare for modeling
 df = teamsFiltered2[teamsFiltered2['Year'] != 2017]
@@ -231,7 +230,7 @@ df.set_index(df['player_index'], inplace = True)
 df.drop(columns = ['player_index'], inplace = True)
 
 #remove redundant variables that may display collinearity
-drop = ['Player','Year','Tm','ALL_STAR','G','MP','GS','3PAr','FTr','WS/48','FG','FGA','3P','3PA','2P','2PA','FT','FTA']
+drop = ['Player','Year','Tm','ALL_STAR','G','MP','GS','3PAr','FTr','TRB%','WS','WS/48','BPM','FG','FGA','3P','3PA','2P','2PA','FT','FTA','ORB','DRB','TRB','AST','STL','BLK','TOV','PTS']
 features = df.drop(columns = drop).columns
 x = df[features]
 y = df['ALL_STAR']
@@ -240,22 +239,22 @@ print(x.shape)
 y.shape
 
 #scale the data (which creates an array) and convert array to df, then QC
-x_scaled = StandardScaler().fit_transform(x.values)
-x_scaled_df = pd.DataFrame(x_scaled, index=x.index, columns=x.columns)
-x.shape
-x_scaled_df.shape
+#x_scaled = StandardScaler().fit_transform(x.values)
+#x_scaled_df = pd.DataFrame(x_scaled, index=x.index, columns=x.columns)
+#x.shape
+#x_scaled_df.shape
 
 #### LOGISTIC REGRESSION ####
 #create an instance of the Logistic regression function, and set max iterations to 200
 #to allow for the model to converge
-logreg=LogisticRegression(max_iter=200)
+logreg=LogisticRegression(solver='liblinear',max_iter=200)
 
 # Create the RFECV object in order to determine the variables to keep based on accuracy
-rfecv = RFECV(estimator=logreg, step=1, scoring='accuracy')
-rfecv.fit(x_scaled_df, y)
+rfecv = RFECV(estimator=logreg, step=1, scoring='accuracy',cv=3)
+rfecv.fit(x, y)
 
 print("Optimal number of features: %d" % rfecv.n_features_)
-print('Selected features: %s' % list(x_scaled_df.columns[rfecv.support_]))
+print('Selected features: %s' % list(x.columns[rfecv.support_]))
 
 #plot the number of features vs. cross-validation scores
 plt.figure(figsize=(10,6))
@@ -266,91 +265,15 @@ plt.show()
 
 
 #modify data to include only the optimal features
-selected_features = list(x_scaled_df.columns[rfecv.support_])
-x_scaled_filtered = x_scaled_df[selected_features]
+selected_features = list(x.columns[rfecv.support_])
+#x_scaled_filtered = x_scaled_df[selected_features]
 y = df['ALL_STAR']
-
-#descriptive summary of scaled data, with p-values
-logit_model=sm.Logit(y,x_scaled_filtered)
-result=logit_model.fit()
-print(result.summary2())
-
-#split the data into training and testing data
-x_train, x_test, y_train, y_test = train_test_split(x_scaled_filtered,y,
-                                                    test_size=0.3, random_state=0)
-
-#create the model
-logisreg = LogisticRegression()
-model_res = logisreg.fit(x_train,y_train)
-logisreg.fit(x_train,y_train)
-
-#evaluate model with test split
-y_pred = logisreg.predict(x_test)
-print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logisreg.score(x_test, y_test)))
-
-conf_matrix = confusion_matrix(y_test, y_pred)
-print(conf_matrix)
-print(classification_report(y_test, y_pred))
-
-#confusion matrix chart
-group_names = ['True Neg','False Pos','False Neg', 'True Pos']
-group_precisions = ['Precision = 0.98', '', '', 'Precision = 0.82']
-group_counts = ["{0:0.0f}".format(value) for value in
-                conf_matrix.flatten()]
-group_percentages = ["{0:.2%}".format(value) for value in
-                     conf_matrix.flatten()/np.sum(conf_matrix)]
-labels = [f"{v1}\n{v2}\n{v3}\n{v4}" for v1, v2, v3, v4 in
-          zip(group_names,group_counts,group_percentages, group_precisions)]
-labels = np.asarray(labels).reshape(2,2)
-sns.axes_style("dark")
-sns.heatmap(conf_matrix, annot=labels, fmt='', linewidths=0.9, cmap='Blues', linecolor="dimgray", annot_kws={"size":12},
-            robust=True)
-
-#Testing Logistic Regression
-model_res.predict(x_train)
-x_train['ALL_STAR_PROB'] = model_res.predict_proba(x_train)[:,1]
-x_train['ALL_STAR'] = y_train
-
-# Predicted Probability of becoming an All-Star
-x_test['ALL_STAR_PROB'] = model_res.predict_proba(x_test)[:, 1]
-
-x_test['ALL_STAR'] = y_test
-
-# Combine the training and testing data sets
-train_df = pd.concat([x_train, x_test])
-
-
-train_df['ALL_STAR_PROB'] = np.round(train_df['ALL_STAR_PROB'], 2)
-
-train_df['player_index'] = train_df.index
-
-train_df[['Player','Year']] = train_df['player_index'].str.split(': ', expand = True)
-
-train_df.drop(columns = ['player_index'], inplace = True)
-
-train_df.to_csv('Probability_DF_All_Star.csv')
-# Players most likely to become an All-Star from training set
-train_df.sort_values(by = 'ALL_STAR_PROB', ascending = False).head(10)
-
-test_df[selected_features].head()
-test_proba = test_df[selected_features]
-
-model_res.predict(test_proba)
-model_res.predict_proba(test_proba)[:5]
-
-all_star_proba = model_res.predict_proba(test_proba)[:,1]
-all_star_proba[:15]
-
-test_df['ALL_STAR_PROB'] = np.round(all_star_proba,2)
-
-# Players most likely to become an All-Star from testing set
-test_df.sort_values(by = 'ALL_STAR_PROB', ascending = False).head(10)
-
+x = df[selected_features]
 
 
 #### RANDOM FOREST TEST ####
 
-x_train, x_test, y_train, y_test = train_test_split(x_scaled_filtered, y, random_state = 42, stratify = y)
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 42, stratify = y)
 
 # Random Forest
 rf = RandomForestClassifier(random_state = 42)
@@ -437,7 +360,7 @@ test_df.sort_values(by = 'ALL_STAR_PROB', ascending = False).head(10)
 
 
 
-best_features = pd.DataFrame(rf_best.feature_importances_.reshape(1,11), columns = selected_features).T
+best_features = pd.DataFrame(rf_best.feature_importances_.reshape(1,22), columns = selected_features).T
 best_features.rename(columns = {0: 'feature_importance'}, inplace = True)
 best_features = best_features.sort_values(by = 'feature_importance', ascending = False)
 
@@ -449,27 +372,113 @@ plt.figure(figsize=(15,10))
 plt.bar(best_features.index,best_features.feature_importance)
 plt.title('Feature Importance', fontsize=20)
 
+
+
+# LOGISTIC REGRESSION
+
+top_features = ['PER', 'PPG', 'VORP', 'OWS', 'USG%', 'DWS', 'OBPM', 'AST%', 'ORB%']
+y = df['ALL_STAR']
+x = x[top_features]
+
+
+
+#descriptive summary of scaled data, with p-values
+logit_model=sm.Logit(y,x)
+result=logit_model.fit()
+print(result.summary2())
+
+#split the data into training and testing data
+x_train, x_test, y_train, y_test = train_test_split(x,y,
+                                                    test_size=0.3, random_state=0)
+
+#create the model
+logisreg = LogisticRegression()
+model_res = logisreg.fit(x_train,y_train)
+logisreg.fit(x_train,y_train)
+
+#evaluate model with test split
+y_pred = logisreg.predict(x_test)
+print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logisreg.score(x_test, y_test)))
+
+conf_matrix = confusion_matrix(y_test, y_pred)
+print(conf_matrix)
+print(classification_report(y_test, y_pred))
+
+#confusion matrix chart
+group_names = ['True Neg','False Pos','False Neg', 'True Pos']
+group_precisions = ['Precision = 0.97', '', '', 'Precision = 0.80']
+group_counts = ["{0:0.0f}".format(value) for value in
+                conf_matrix.flatten()]
+group_percentages = ["{0:.2%}".format(value) for value in
+                     conf_matrix.flatten()/np.sum(conf_matrix)]
+labels = [f"{v1}\n{v2}\n{v3}\n{v4}" for v1, v2, v3, v4 in
+          zip(group_names,group_counts,group_percentages, group_precisions)]
+labels = np.asarray(labels).reshape(2,2)
+sns.axes_style("dark")
+sns.heatmap(conf_matrix, annot=labels, fmt='', linewidths=0.9, cmap='Blues', linecolor="dimgray", annot_kws={"size":12},
+            robust=True)
+
+#Testing Logistic Regression
+model_res.predict(x_train)
+x_train['ALL_STAR_PROB'] = model_res.predict_proba(x_train)[:,1]
+x_train['ALL_STAR'] = y_train
+
+# Predicted Probability of becoming an All-Star
+x_test['ALL_STAR_PROB'] = model_res.predict_proba(x_test)[:, 1]
+
+x_test['ALL_STAR'] = y_test
+
+# Combine the training and testing data sets
+train_df = pd.concat([x_train, x_test])
+
+
+train_df['ALL_STAR_PROB'] = np.round(train_df['ALL_STAR_PROB'], 2)
+
+train_df['player_index'] = train_df.index
+
+train_df[['Player','Year']] = train_df['player_index'].str.split(': ', expand = True)
+
+train_df.drop(columns = ['player_index'], inplace = True)
+
+train_df.to_csv('Probability_DF_All_Star.csv')
+# Players most likely to become an All-Star from training set
+train_df.sort_values(by = 'ALL_STAR_PROB', ascending = False).head(10)
+
+test_df[top_features].head()
+test_proba = test_df[top_features]
+
+model_res.predict(test_proba)
+model_res.predict_proba(test_proba)[:5]
+
+all_star_proba = model_res.predict_proba(test_proba)[:,1]
+all_star_proba[:15]
+
+test_df['ALL_STAR_PROB'] = np.round(all_star_proba,2)
+
+# Players most likely to become an All-Star from testing set
+test_df.sort_values(by = 'ALL_STAR_PROB', ascending = False).head(10)
+
+
+
+# Visualizations
+
 # Checking the top two features - clearly the top right are reserved for All-Stars
 plt.figure(figsize=(15,10))
-plt.scatter(teamsFiltered2.PER,teamsFiltered2.WS,c=teamsFiltered2.ALL_STAR)
-plt.title('PER Against WS', fontsize=20)
+sns.lmplot('PER', 'VORP', teamsFiltered2, hue='ALL_STAR', fit_reg=False)
+plt.title('PER Against VORP', fontsize=20)
 plt.xlabel('PER', fontsize=16)
-plt.ylabel('WS', fontsize=16)
+plt.ylabel('VORP', fontsize=16)
+fig = plt.gcf()
+fig.set_size_inches(15, 10)
+plt.show()
 
-# Again, clear distinction between All Stars' PER and others
-plt.figure(figsize=(15,10))
-plt.scatter(teamsFiltered2.PER,teamsFiltered2.MP,c=teamsFiltered2.ALL_STAR)
-plt.title('PER Against Minutes Played', fontsize=20)
-plt.xlabel('PER', fontsize=16)
-plt.ylabel('MP', fontsize=16)
 
-# PTS scored also favors the All Stars, but there are more non-All Stars close to All-Star level
+# Checking PTS against MP - all-stars are slightly better but less clearly
 plt.figure(figsize=(15,10))
-plt.scatter(teamsFiltered2.PTS,teamsFiltered2.MP,c=teamsFiltered2.ALL_STAR)
-plt.title('PTS Against Minutes Played', fontsize=20)
+sns.lmplot('PTS', 'MP', teamsFiltered2, hue='ALL_STAR', fit_reg=False)
+plt.title('PTS Against MP', fontsize=20)
 plt.xlabel('PTS', fontsize=16)
 plt.ylabel('MP', fontsize=16)
-
-
-
-
+fig = plt.gcf()
+fig.set_size_inches(15, 10)
+plt.show()
